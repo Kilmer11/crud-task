48f0e699cefe330507c195b60d2bfa4589ca5d2f
Kilmer <kilmerandrade45@gmail.com>
2025-10-12 14:38:19 -0300

    "First commit"

--- src/features/auth/types/authModel.ts
+++ src/features/auth/types/authModel.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type AuthModel = {
-  user: any[];
+  user: { user: string; email: string }[];
   isLoggedIn: boolean;
   isLoading: boolean;
   error: any;
